3debbe683d438f952551db596e215b815cdeee80
mateogon <mateogon97@hotmail.com>
2025-07-03 16:55:15 -0400

    feat: enable CORS in main application setup for frontend access

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+// auth-backend/src/main.ts
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilitar CORS
+  app.enableCors({
+    origin: 'http://localhost:3001', // O el puerto en el que corre tu frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
